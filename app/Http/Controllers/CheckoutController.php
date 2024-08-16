010ae4101e1347ace4ef8b7ad5ef589c704e49f6
Md.Jahid Hasan <jahidhasan5803733@gmail.com>
2024-08-17 01:59:36 +0600

    Check stock availability | Stock validation in checkout page

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cart;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -10,7 +11,26 @@ class CheckoutController extends Controller
 {
     public function checkout()
     {
+
+        
+        $old_cartItems=Cart::where('user_id',Auth::id())->get();
+        foreach($old_cartItems as $cart)
+        {
+
+            if(!Product::where('id',$cart->prod_id)->where('quantity','>=',$cart->prod_qty)->exists())
+            {
+            $removeItem=Cart::Where('user_id',Auth::id())->where('prod_id',$cart->prod_id)->first();
+            $removeItem->delete();
+            }
+            }
         $cartItems=Cart::where('user_id',Auth::id())->get();
+
+       
         return view('fronted.checkout',compact('cartItems'));
+    }
+
+    public function placeorder(Request $request)
+    {
+
     }
 }
